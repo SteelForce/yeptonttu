24084b8d70caf7662a76f93dabeba1c6be3b1df1
SteelForce <36268348+SteelForce@users.noreply.github.com>
2020-10-04 00:10:11 +0300

    all

--- commands/tonttuhelp.js
+++ commands/tonttuhelp.js
@@ -3,6 +3,6 @@ module.exports = {
     name: 'tonttuhelp',
     description: "this is an info command!",
     execute(message, args){
-        message.channel.send('currently: !tonttupallo, !tonttufeedaa, !ping, !doinb, !kentry, !vaihtoehdot, !a, !b, !c', '\n','VOICE: !ratearl, !1v9, !jimi_irl and !dece', '\n', 'and !tonttuhelp');
+        message.channel.send('currently: !tonttupallo !tonttufeedaa !ping !doinb !kentry !vaihtoehdot !a !b !c !ratearl !1v9 !jimi_irl !dece and !tonttuhelp');
     }
 }
