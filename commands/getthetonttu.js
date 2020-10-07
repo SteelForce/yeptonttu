138cf0b7d88b8df8d6e94dbf11981135748c0401
SteelForce <36268348+SteelForce@users.noreply.github.com>
2020-10-07 13:32:08 +0300

    Create getthetonttu.js

---
+++ commands/getthetonttu.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    name: 'getthetonttu',
+    description: "this is a petri n command!",
+    execute(message, args){
+        message.channel.send('https://discord.com/oauth2/authorize?client_id=739179628236767272&scope=bot&permissions=8');
+    }
+}
